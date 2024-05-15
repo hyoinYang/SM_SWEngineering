53dc185609d6d51c8cb4d969f18f6f343b60271f
hyoinYang <hyoinyang02@gmail.com>
2024-05-16 00:41:31 +0900

    fix: login, word_Management GUI

--- dictionary/login.py
+++ dictionary/login.py
@@ -38,26 +38,45 @@ class LoginPage:
         sidebar_icon = tk.PhotoImage(file="dictionary/assets/frame2/title_bar_frame_image/sidebar_icon.png").subsample(10)
         sidebar_button = tk.Button(title_bar_frame, image=sidebar_icon, relief="flat", bd=0, command=lambda:open_sidebar_window(root), bg="#838383", cursor="hand2")
         sidebar_button.image = sidebar_icon
-        sidebar_button.pack(side="right", padx=5)
+        sidebar_button.pack(side="right", padx=10)
 
         # 사용자 이름 라벨 및 텍스트 상자
-        self.username_label = tk.Label(self.root, text="사용자 이름:")
-        self.username_label.pack(pady=5)
-        self.username_entry = tk.Entry(self.root)
-        self.username_entry.pack(pady=5)
+        username_frame = tk.Frame(self.root, relief="solid", borderwidth=1, highlightbackground="gray", highlightcolor="gray")
+        username_frame.pack(pady=10)
+
+        username_icon = tk.PhotoImage(file="resource/username_entry.png").subsample(1)
+        self.username_label = tk.Label(username_frame, image=username_icon, relief="solid", bd=0, cursor="hand2")
+        self.username_label.image = username_icon
+        self.username_label.pack(side="left", padx=5)
+        
+        self.username_entry = tk.Entry(username_frame, relief="flat", bg="#F0F0F0")
+        self.username_entry.pack(side="left", padx=5)
+        self.username_entry.insert(0, "Username")
 
         # 비밀번호 라벨 및 텍스트 상자
-        self.password_label = tk.Label(self.root, text="비밀번호:")
-        self.password_label.pack(pady=5)
-        self.password_entry = tk.Entry(self.root, show="*")
-        self.password_entry.pack(pady=5)
+        password_frame = tk.Frame(self.root, relief="solid", borderwidth=1, highlightbackground="gray", highlightcolor="gray")
+        password_frame.pack(pady=10)
+
+        password_icon = tk.PhotoImage(file="resource/password_entry.png").subsample(1)
+        self.password_label = tk.Label(password_frame, image=password_icon, relief="flat", bd=0)
+        self.password_label.image = password_icon
+        self.password_label.pack(side="left", padx=5)
+        self.password_entry = tk.Entry(password_frame, relief="flat", bg="#F0F0F0")
+        self.password_entry.pack(side="left", padx=5)
+        self.password_entry.insert(0, "password")
 
         # 로그인 버튼
-        login_button = tk.Button(self.root, text="로그인", command=self.validate_login, bg="#838383")
-        login_button.pack(pady=10)
+        login_icon = tk.PhotoImage(file="resource/login_btn.png").subsample(2)
+        login_button = tk.Button(self.root, image=login_icon, relief="flat", bd=0, command=self.validate_login, cursor="hand2")
+        login_button.image = login_icon
+  
+        login_button.pack(pady=20)
 
         # 회원가입 버튼
-        signup_button = tk.Button(self.root, text="회원가입", command=self.validate_signup, bg="#838383")
+        signup_icon = tk.PhotoImage(file="resource/signup_btn.png").subsample(2)
+        signup_button = tk.Button(self.root, image=signup_icon, relief="flat", bd=0, command=self.validate_login, cursor="hand2")
+        signup_button.image = signup_icon
+
         signup_button.pack(pady=10)
 
     # -------------------------------- 버튼의 이벤트 --------------------------------
--- dictionary/word_management.py
+++ dictionary/word_management.py
@@ -10,6 +10,65 @@ def upload_csv_file():
     print("csv 파일 업로드 완")
 
 
+# -------------------------------- 엔트리 관련 함수 --------------------------------
+
+# 삭제하기
+def delete_enter(event):
+    # 입력 상자에서 입력된 내용 가져오기
+    word = del_entry.get()
+    
+    # 입력된 내용을 터미널에 출력하기
+    if (word == "삭제할 단어"):
+        print("ERROR: 모든 단어 입력 X")
+    else:
+        print("Delete input:", word) 
+
+        # 입력 상자 초기화
+        del_entry.delete(0, "end")
+        del_entry.insert(0, "삭제할 단어")
+
+# 수정하기
+def modify_enter(event):
+    # 입력 상자에서 입력된 내용 가져오기
+    word = modify_word_entry.get()
+    korean=modify_korean_entry.get()
+    example = modify_ex_entry.get()
+
+    # 입력된 내용을 터미널에 출력하기
+    if (word == "추가할 단어" or korean == "추가할 단어의 뜻" or example == "추가할 단어의 예문"):
+        print("ERROR: 모든 단어 입력 X")
+    else:
+        print("Modify input) word = %s, korean = %s, example = %s", word, korean, example)
+        # 입력 상자 초기화
+        add_word_entry.delete(0, "end")
+        add_word_entry.insert(0, "추가할 단어")
+        add_korean_entry.delete(0, "end")
+        add_korean_entry.insert(0, "추가할 단어의 뜻")
+        add_ex_entry.delete(0, "end")
+        add_ex_entry.insert(0, "추가할 단어의 예문")
+
+
+# 추가하기
+def add_enter(event):
+    # 입력 상자에서 입력된 내용 가져오기
+    word = add_word_entry.get()
+    korean=add_korean_entry.get()
+    example = add_ex_entry.get()
+
+    # 입력된 내용을 터미널에 출력하기
+    if (word == "추가할 단어" or korean == "추가할 단어의 뜻" or example == "추가할 단어의 예문"):
+        print("ERROR: 모든 단어 입력 X")
+    else:
+        print("Add input) word = %s, korean = %s, example = %s", word, korean, example)
+        # 입력 상자 초기화
+        add_word_entry.delete(0, "end")
+        add_word_entry.insert(0, "추가할 단어")
+        add_korean_entry.delete(0, "end")
+        add_korean_entry.insert(0, "추가할 단어의 뜻")
+        add_ex_entry.delete(0, "end")
+        add_ex_entry.insert(0, "추가할 단어의 예문")
+
+
 # -------------------------------- GUI 초기화 --------------------------------
 root = tk.Tk()
 root.title("단어 관리")
@@ -43,66 +102,72 @@ canvas.pack(side="left", fill="both", expand=True)
 
 # 내부 프레임 생성
 frame = tk.Frame(canvas)
-canvas.create_window((30, 0), window=frame, anchor="nw")
+canvas.create_window((50, 0), window=frame, anchor="nw")
 
 # 회색 박스 생성
-gray_box = tk.Frame(frame, bg="#838383")
-gray_box.pack(pady=5, fill="x")
+delete_box = tk.Frame(frame, bg="#838383")
+delete_box.pack(pady=30, fill="x")
 
-textTest="단어 삭제하기"
+# 단어 삭제 칸
+del_text_box = tk.Label(delete_box, text="단어 삭제하기", font=("Helvetica", 15), bg="#838383")
+del_text_box.pack(padx=5, pady=10)
 
-# 텍스트 박스 생성
-text_box = tk.Text(gray_box, bg="#838383", fg="white", wrap="word", height=4, padx=20, pady=20)
-text_box.insert("end", textTest)
-text_box.config(state="disabled")
-text_box.pack(side="top", fill="both", expand=True)
-
-# 입력 상자 생성
-input_box = tk.Entry(gray_box, bg="white", fg="black", bd=2)
-input_box.pack(side="bottom", fill="x", padx=20, pady=10)
-
+del_entry = tk.Entry(delete_box, bg="white", fg="black", bd=2, relief="flat")
+del_entry.insert(0, "삭제할 단어")
+del_entry.pack(padx=20, pady=10)
+del_entry.bind("<Return>", delete_enter)
 
 # 단어 추가 칸
-gray_box = tk.Frame(frame, bg="gray")
-gray_box.pack(pady=5, fill="x")
+add_box = tk.Frame(frame, bg="gray")
+add_box.pack(pady=30, fill="x")
 
-textTest="단어 추가하기"
+add_text_box = tk.Label(add_box, text="단어 추가하기", font=("Helvetica", 15), bg="#838383")
+add_text_box.pack(padx=20, pady=10)
 
-text_box = tk.Text(gray_box, bg="gray", fg="white", wrap="word", height=4, padx=20, pady=20)
-text_box.insert("end", textTest)
-text_box.config(state="disabled")
-text_box.pack(side="top", fill="both", expand=True)
+add_word_entry = tk.Entry(add_box, bg="white", fg="black", bd=2, relief="flat")
+add_word_entry.insert(0, "추가할 단어")
+add_word_entry.pack(side="left", padx=20, pady=10)
+add_word_entry.bind("<Return>", add_enter)
 
-# 입력 상자 생성
-input_box = tk.Entry(gray_box, bg="white", fg="black", bd=2)
-input_box.pack(side="left", fill="x", padx=20, pady=10)
+add_korean_entry = tk.Entry(add_box, bg="white", fg="black", bd=2, relief="flat")
+add_korean_entry.insert(0, "추가할 단어의 뜻")
+add_korean_entry.pack(side="left", fill="x", padx=20, pady=10)
+add_korean_entry.bind("<Return>", add_enter)
 
-# 입력 상자 생성
-input_box = tk.Entry(gray_box, bg="white", fg="black", bd=2)
-input_box.pack(side="right", fill="x", padx=20, pady=10)
+add_ex_entry = tk.Entry(add_box, bg="white", fg="black", bd=2, relief="flat")
+add_ex_entry.insert(0, "추가할 단어의 예문")
+add_ex_entry.pack(side="left", fill="x", padx=20, pady=10)
+add_ex_entry.bind("<Return>", add_enter)
 
 # 단어 수정 칸
-gray_box = tk.Frame(frame, bg="gray")
-gray_box.pack(pady=5, fill="x")
+modify_box = tk.Frame(frame, bg="gray")
+modify_box.pack(pady=30, fill="x")
 
-textTest="단어 수정하기"
+text_box = tk.Label(modify_box, text="단어 수정하기", font=("Helvetica", 15), bg="#838383")
+text_box.pack(padx=20, pady=10)
 
-text_box = tk.Text(gray_box, bg="gray", fg="white", wrap="word", height=4, padx=20, pady=20)
-text_box.insert("end", textTest)
-text_box.config(state="disabled")
-text_box.pack(side="top", fill="both", expand=True)
+# 입력 상자(수정할 단어) 생성
+modify_word_entry = tk.Entry(modify_box, bg="white", fg="black", bd=2, relief="flat")
+modify_word_entry.pack(side="left", fill="x", padx=20, pady=10)
+modify_word_entry.insert(0, "수정할 단어")
+modify_word_entry.bind("<Return>", modify_enter)
 
-# 입력 상자 생성
-input_box = tk.Entry(gray_box, bg="white", fg="black", bd=2)
-input_box.pack(side="left", fill="x", padx=20, pady=10)
+# 입력 상자(수정할 내용) 생성
+modify_korean_entry = tk.Entry(modify_box, bg="white", fg="black", bd=2, relief="flat")
+modify_korean_entry.pack(side="left", fill="x", padx=20, pady=10)
+modify_korean_entry.insert(0, "수정할 단어의 뜻")
+modify_korean_entry.bind("<Return>", modify_enter)
 
-# 입력 상자 생성
-input_box = tk.Entry(gray_box, bg="white", fg="black", bd=2)
-input_box.pack(side="right", fill="x", padx=20, pady=10)
+modify_ex_entry = tk.Entry(modify_box, bg="white", fg="black", bd=2, relief="flat")
+modify_ex_entry.pack(side="left", fill="x", padx=20, pady=10)
+modify_ex_entry.insert(0, "수정할 단어의 예문")
+modify_ex_entry.bind("<Return>", modify_enter)
 
 # CSV 파일 업로드 버튼
-login_button = tk.Button(canvas, text="csv파일 업데이트하기", command=lambda:upload_csv_file, bg="#838383")
-login_button.pack(side="bottom", pady=30)
+upload_csv_icon = tk.PhotoImage(file="resource/csv_upload_btn.png").subsample(2)
+upload_csv_button = tk.Button(canvas, image=upload_csv_icon, relief="flat", bd=0, command=lambda:upload_csv_file(), cursor="hand2")
+upload_csv_button.image = upload_csv_icon
+upload_csv_button.place(x=500, y=450)
 
 
 
