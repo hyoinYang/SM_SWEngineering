ebd3bdf234cab5ea7d241aa5a65cffef5efc6467
arongsnuna <unhi000@naver.com>
2024-05-20 22:26:11 +0900

    add_studied_word 추가

--- addBookmark.py
+++ addBookmark.py
@@ -9,3 +9,9 @@ def add_bookmark(mem_id, eng_word):
     mycursor.execute(insert_query, val)
     print("북마크에 추가했습니다.")
     return True
+
+def add_studied_word(mem_id, eng_word):
+    insert_query = "INSERT INTO Studied_words (eng_word, member_id) VALUES (%s, %s)"
+    val = (eng_word, mem_id)
+    mycursor.execute(insert_query, val)
+    return True
