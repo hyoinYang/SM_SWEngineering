8f169a3a34cc4f7e3f714953b9d01cfffde40b33
hyoinYang <hyoinyang02@gmail.com>
2024-05-30 00:29:39 +0900

    fix: linking word_management

--- dictionary/word_management.py
+++ dictionary/word_management.py
@@ -5,12 +5,11 @@ from PIL import Image, ImageTk
 
 # ----------------------------- Model -----------------------------
 class ManagementModel:
-    def __init__(self):
-        gear_icon_path = "resource/gear_icon.png"
-        menu_icon_path = "resource/sidebar_icon.png"
+    gear_icon_path = "resource/gear_icon.png"
+    menu_icon_path = "resource/sidebar_icon.png"
     
     # 삭제하기
-    def delete_enter(event, del_entry):
+    def delete_enter(del_entry):
         # 입력 상자에서 입력된 내용 가져오기
         word = del_entry.get()
         
@@ -25,7 +24,7 @@ class ManagementModel:
             del_entry.insert(0, "삭제할 단어")
 
     # 수정하기
-    def modify_enter(event, modify_word_entry, modify_korean_entry, modify_ex_entry):
+    def modify_enter(modify_word_entry, modify_korean_entry, modify_ex_entry):
         # 입력 상자에서 입력된 내용 가져오기
         word = modify_word_entry.get()
         korean=modify_korean_entry.get()
@@ -46,7 +45,7 @@ class ManagementModel:
 
 
     # 추가하기
-    def add_enter(event, add_word_entry, add_korean_entry, add_ex_entry):
+    def add_enter(add_word_entry, add_korean_entry, add_ex_entry):
         # 입력 상자에서 입력된 내용 가져오기
         word = add_word_entry.get()
         korean=add_korean_entry.get()
@@ -72,31 +71,36 @@ class ManagementView:
         self.setup_ui()
 
     def setup_ui(self):
+
         # -------------------------------- GUI 초기화 --------------------------------
-        root = tk.Tk()
+        """root = tk.Tk()
         root.title("단어 관리")
         root.geometry("700x550")
-        root.configure(background="#FFFFFF")  
-
+        root.configure(background="#FFFFFF")  """
+        root = self.root
         style = ttk.Style()
         style.theme_use('clam')
 
+
         # -------------------------------- 회색 바 설정 --------------------------------
+        """        
         style.configure('TFrame', background='#838383')
-        title_bar_frame = ttk.Frame(root, style='TFrame', height=30)
-        title_bar_frame.pack(fill='x')
+            title_bar_frame = ttk.Frame(root, style='TFrame', height=30)
+            title_bar_frame.pack(fill='x')
 
-        gear_icon = ImageTk.PhotoImage(Image.open(ManagementModel.gear_icon_path).resize((30, 30)))
-        menu_icon = ImageTk.PhotoImage(Image.open(ManagementModel.menu_icon_path).resize((30, 30)))
+            gear_icon = ImageTk.PhotoImage(Image.open(ManagementModel.gear_icon_path).resize((30, 30)))
+            menu_icon = ImageTk.PhotoImage(Image.open(ManagementModel.menu_icon_path).resize((30, 30)))
 
-        gear_button = tk.Button(title_bar_frame, image=gear_icon, borderwidth=0, bg="#838383", cursor="hand2")
-        gear_button.pack(side=tk.LEFT, padx=20)
+            gear_button = tk.Button(title_bar_frame, image=gear_icon, borderwidth=0, bg="#838383", cursor="hand2")
+            gear_button.pack(side=tk.LEFT, padx=20)
 
-        text_label = tk.Label(title_bar_frame, text="단어 관리", font=("Helvetica", 15, "bold"), background="#838383")
-        text_label.pack(side="left", padx=5)
+            text_label = tk.Label(title_bar_frame, text="단어 관리", font=("Helvetica", 15, "bold"), background="#838383")
+            text_label.pack(side="left", padx=5)
 
-        menu_button = tk.Button(title_bar_frame, image=menu_icon, borderwidth=0, bg="#838383", cursor="hand2")
-        menu_button.pack(side=tk.RIGHT, padx=20)
+            menu_button = tk.Button(title_bar_frame, image=menu_icon, borderwidth=0, bg="#838383", cursor="hand2")
+            menu_button.pack(side=tk.RIGHT, padx=20)
+        """ 
+        
 
         # -------------------------------- 내용 --------------------------------
         # Canvas 생성
@@ -130,12 +134,12 @@ class ManagementView:
         add_word_entry = tk.Entry(add_box, bg="white", fg="black", bd=2, relief="flat")
         add_word_entry.insert(0, "추가할 단어")
         add_word_entry.pack(side="left", padx=20, pady=10)
-        add_word_entry.bind("<Return>", ManagementModel.add_enter(add_word_entry, add_korean_entry, add_ex_entry))
+        #add_word_entry.bind("<Return>", ManagementModel.add_enter(add_word_entry, add_korean_entry, add_ex_entry))
 
         add_korean_entry = tk.Entry(add_box, bg="white", fg="black", bd=2, relief="flat")
         add_korean_entry.insert(0, "추가할 단어의 뜻")
         add_korean_entry.pack(side="left", fill="x", padx=20, pady=10)
-        add_korean_entry.bind("<Return>", ManagementModel.add_enter(add_word_entry, add_korean_entry, add_ex_entry))
+        #add_korean_entry.bind("<Return>", ManagementModel.add_enter(add_word_entry, add_korean_entry, add_ex_entry))
 
         add_ex_entry = tk.Entry(add_box, bg="white", fg="black", bd=2, relief="flat")
         add_ex_entry.insert(0, "추가할 단어의 예문")
@@ -153,13 +157,13 @@ class ManagementView:
         modify_word_entry = tk.Entry(modify_box, bg="white", fg="black", bd=2, relief="flat")
         modify_word_entry.pack(side="left", fill="x", padx=20, pady=10)
         modify_word_entry.insert(0, "수정할 단어")
-        modify_word_entry.bind("<Return>", ManagementModel.modify_enter(modify_word_entry, modify_korean_entry, modify_ex_entry))
+        #modify_word_entry.bind("<Return>", ManagementModel.modify_enter(modify_word_entry, modify_korean_entry, modify_ex_entry))
 
         # 입력 상자(수정할 내용) 생성
         modify_korean_entry = tk.Entry(modify_box, bg="white", fg="black", bd=2, relief="flat")
         modify_korean_entry.pack(side="left", fill="x", padx=20, pady=10)
         modify_korean_entry.insert(0, "수정할 단어의 뜻")
-        modify_korean_entry.bind("<Return>", ManagementModel.modify_enter(modify_word_entry, modify_korean_entry, modify_ex_entry))
+        #modify_korean_entry.bind("<Return>", ManagementModel.modify_enter(modify_word_entry, modify_korean_entry, modify_ex_entry))
 
         modify_ex_entry = tk.Entry(modify_box, bg="white", fg="black", bd=2, relief="flat")
         modify_ex_entry.pack(side="left", fill="x", padx=20, pady=10)
@@ -167,8 +171,9 @@ class ManagementView:
         modify_ex_entry.bind("<Return>", ManagementModel.modify_enter(modify_word_entry, modify_korean_entry, modify_ex_entry))
 
         # CSV 파일 업로드 버튼
-        upload_csv_icon = tk.PhotoImage(file="resource/csv_upload_btn.png").subsample(2)
-        upload_csv_button = tk.Button(canvas, image=upload_csv_icon, relief="flat", bd=0, command=lambda:ManagementController.upload_csv_file(), cursor="hand2")
+        upload_csv_icon = tk.PhotoImage(file="resource\csv_upload_btn.png").subsample(2)
+        upload_csv_button = tk.Button(canvas, relief = "flat", bd = 0, text="csv 파일 업로드", command=lambda:ManagementController.upload_csv_file(), cursor="hand2")
+        #upload_csv_button = tk.Button(canvas, image=upload_csv_icon, relief="flat", bd=0, command=lambda:ManagementController.upload_csv_file(), cursor="hand2")
         upload_csv_button.image = upload_csv_icon
         upload_csv_button.place(x=500, y=450)
 
@@ -183,4 +188,8 @@ class ManagementController:
 
     def upload_csv_file():
         print("csv 파일 업로드 완")
-    
+
+"""if __name__ == "__main__":
+    root = tk.Tk()
+    ManagementController(root)
+    root.mainloop()"""
